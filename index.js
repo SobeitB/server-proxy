5e19b294aa6fe2920af4e92ef431f5ca1cc55f7a
-evgeny <eb8718541@gmail.com>
2023-02-22 12:41:05 +0300

    prod

--- index.js
+++ index.js
@@ -1,53 +1,65 @@
 
 const express = require("express");
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const cors = require('cors')
-
+const fetch = require('node-fetch');
+const cors = require('cors') 
 
 const app = express();
 app.use(cors({
     origin: '*'
 }))
 
-app.use('/api', (req, res) => {
-    res.json({hello:'hello'})
-});
+app.get('/ping', (req, res) => {
+  res.json({hello:'ping'})
+})
+
+app.get('/allPools/:network/:page', async ({params}, res) => {
+  const {network, page} = params;
+  let pools = await fetch(`https://app.geckoterminal.com/api/p1/${network}/pools?page=${page}&include=dex`);
+  pools = await pools.json();
+
+  res.json(pools)
+})
+
+app.get('/trends/:network', async ({params}, res) => {
+  const {network} = params;
+  let trends = await fetch(`https://app.geckoterminal.com/api/p1/trends?network=${network}`);
+
+  res.json(await trends.json())
+})
+
+app.get('/pool/:network/:address', async ({params}, res) => {
+  const {network, address} = params;
+  let pools = await fetch(`https://app.geckoterminal.com/api/p1/${network}/pools/${address}?include=dex`);
+  pools = await pools.json();
+
+  res.json(pools)
+})
+
+app.get('/poolTx/:network/:address/:page', async ({params}, res) => {
+  const {network, address, page} = params;
+  let pools = await fetch(`https://app.geckoterminal.com/api/p1/${network}/pools/${address}/swaps?include=from_token%2Cto_token&page=${page}`);
+  pools = await pools.json();
+
+  res.json(pools)
+})
+
+app.get('/search/:query', async ({params}, res) => {
+  const {query} = params;
+  let pools = await fetch(`https://app.geckoterminal.com/api/p1/search?query=${query}`);
+  pools = await pools.json();
+
+  res.json(pools)
+})
+
 
 app.listen(3001);
 
-// ALL POOLS
-app.use(
-    '/',
-    createProxyMiddleware({
-      target: 'https://app.geckoterminal.com/api/p1',
-      changeOrigin: true,
-    })
-);
-
-
-app.use(
-    '/trends',
-    createProxyMiddleware({
-      target: 'https://app.geckoterminal.com/api',
-      changeOrigin: true,
-    })
-);
-
-// TAXES
-app.use(
-    '/default',
-    createProxyMiddleware({
-      target: 'рttps://aywt3wreda.execute-api.eu-west-1.amazonaws.com',
-      changeOrigin: true,
-    })
-);
-
 // META POOL
-app.use(
-    '/v1',
-    createProxyMiddleware({
-      target: 'https://uanalytics-api.herokuapp.com/api',
-      changeOrigin: true,
-    })
-);
+// app.use(
+//     '/v1',
+//     createProxyMiddleware({
+//       target: 'https://uanalytics-api.herokuapp.com/api',
+//       changeOrigin: true,
+//     })
+// );
   
